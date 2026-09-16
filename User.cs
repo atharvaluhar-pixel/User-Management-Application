976de9c1dcca0310733bcec98d0018eeb386b019
atharvaluhar-pixel <atharvaluhar@gmail.com>
2026-09-16 15:25:41 +0200

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+    }
+}
